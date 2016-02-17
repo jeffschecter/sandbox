6a95991917c54d53c11ba9cbbfe431f14405089e
jeffschecter <jeff@levelmoney.com>
2016-02-17 15:10:30 -0800

    more colorization stuff

--- load_cifar10.py
+++ load_cifar10.py
@@ -1,27 +1,5 @@
 import cPickle
-import gzip
 import os
-import shutil
-import sys
-import urllib2
-
-
-def MaybeDownload():
-    dataset_url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
-
-    if "cifar10" not in os.listdir("."):
-        os.mkdir("cifar10")
-
-    if "batches.meta" not in os.listdir("./cifar10/"):
-        try:
-            sys.stdout.write("Retrieving tar...")
-            sys.stdout.flush()
-            blob = urllib2.urlopen(dataset_url).read()
-            sys.stdout.write(" fetched...")
-            sys.stdout.flush()
-            #TODO
-        except:
-            pass
 
 
 def LoadBatch(file):
@@ -31,7 +9,6 @@ def LoadBatch(file):
 
 
 def Batches():
-    MaybeDownload()
     prefix = "./cifar10/"
     batches = (
         "data_batch_1",
--- ops.py
+++ ops.py
@@ -1,6 +1,26 @@
 import tensorflow as tf
 
 
+FLOAT = tf.float32
+
+
+# --------------------------------------------------------------------------- #
+# Nonlinearities                                                              #
+# --------------------------------------------------------------------------- #
+def LeakyReLUMaker(leak):
+    def LeakyReLU(x):
+        return tf.maximum(x, 0) + leak * tf.minimum(x, 0)
+    return LeakyReLU
+
+
+def ELU(x):
+    pos = tf.cast(tf.greater_equal(x, 0), FLOAT)
+    return (pos * x) + ((1 - pos) * (tf.exp(x) - 1))
+
+
+# --------------------------------------------------------------------------- #
+# Simple Layer Construction                                                   #
+# --------------------------------------------------------------------------- #
 def Weight(shape, sd=0.02):
     initial = tf.random_normal(shape, mean=0, stddev=sd)
     return tf.Variable(initial, name="weights")
@@ -11,11 +31,49 @@ def Bias(shape, sd=0.02):
     return tf.Variable(initial, name="bias")
 
 
+def HiddenLayer(inp, shape, nonlin=ELU, init=0.001, scope="RBM"):
+    with tf.name_scope(scope) as ns:
+        W = Weight(shape, init=init)
+        b = Bias([shape[1]], init=init)
+        h = nonlin(tf.matmul(inp, W) + b)
+        return W, b, h
+
+
+# --------------------------------------------------------------------------- #
+# Convolutional Layers                                                        #
+# --------------------------------------------------------------------------- #
+def Conv2D(x, W, name="Conv2D"):
+    return tf.nn.conv2d(
+        x, W,
+        strides=[1, 1, 1, 1],
+        padding='SAME',
+        name=name)
+
+
+def ConvLayer(x, filter_size, out_channels, nonlinearity, name):
+    in_channels = int(x.get_shape()[-1])
+    with tf.name_scope(name) as scope:
+        W = Weight([filter_size, filter_size, in_channels, out_channels])
+        b = Bias([out_channels])
+        h = nonlinearity(Conv2D(x, W) + b)
+    return h
+
+
+def MaxPool2x2(x, name="MaxPool"):
+    return tf.nn.max_pool(
+        x,
+        ksize=[1, 2, 2, 1],
+        strides=[1, 2, 2, 1],
+        padding='SAME',
+        name=name)
+
+
 def StridedConv(x, W):
     return tf.nn.conv2d(
         x, W,
         strides=[1, 2, 2, 1],
-        padding="SAME", name="strided_conv")
+        padding="SAME",
+        name="strided_conv")
 
 
 # The function tf.nn.deconv2d is an undecumented part of the TensorFlow API,
@@ -39,8 +97,10 @@ def FractionallyStridedConv(x, W, output_channels=None, batch_size=BATCH_SIZE):
         name="fractionally_strided_conv")
 
 
-# A batch norm that doesn't require running separate update ops!
-# Stolen from http://stackoverflow.com/a/34634291
+# --------------------------------------------------------------------------- #
+# A batch norm that doesn't require running separate update ops!              #
+# Stolen from http://stackoverflow.com/a/34634291                             #
+# --------------------------------------------------------------------------- #
 def BatchNorm(should_train, inp, scope="BatchNorm", affine=True):
     with tf.variable_scope(scope):
         beta = tf.Variable(
@@ -67,13 +127,3 @@ def BatchNorm(should_train, inp, scope="BatchNorm", affine=True):
         return tf.nn.batch_norm_with_global_normalization(
             inp, mean, var, beta, gamma, 0.001, affine)
 
-    
-def LeakyReLUMaker(leak):
-    def LeakyReLU(x):
-        return tf.maximum(x, 0) + leak * tf.minimum(x, 0)
-    return LeakyReLU
-
-
-def ELU(x):
-    pos = tf.cast(tf.greater_equal(x, 0), FLOAT)
-    return (pos * x) + ((1 - pos) * (tf.exp(x) - 1))
