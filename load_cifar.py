2fcfa1aca31e766ac9850c553cc38406d3a7bf68
jeffschecter <jeff@levelmoney.com>
2016-06-17 16:30:07 -0700

    cifar100 beginnings of feedforward one shot learner

--- load_cifar.py
+++ load_cifar.py
@@ -2,12 +2,12 @@ import cPickle
 import os
 
 
-def LoadBatch(file):
-    with open(file, 'rb') as f:
+def LoadBatch(fname, label_kword="labels"):
+    with open(fname, 'rb') as f:
         batch_data = cPickle.load(f)
     arr = batch_data["data"]
-    images = arr.reshape((10000, 3, 32, 32)).transpose(0, 2, 3, 1) / 255.0
-    labels = batch_data["labels"]
+    images = arr.reshape((arr.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1) / 255.0
+    labels = batch_data[label_kword]
     return images, labels
 
 
@@ -15,17 +15,22 @@ def Batches(include_labels=False, dataset="cifar10"):
     prefix = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         dataset)
-    batches = (
-        "data_batch_1",
-        "data_batch_2",
-        "data_batch_3",
-        "data_batch_4",
-        "data_batch_5",
-        "test_batch")
+    if dataset == "cifar10":
+        batches = (
+            "data_batch_1",
+            "data_batch_2",
+            "data_batch_3",
+            "data_batch_4",
+            "data_batch_5",
+            "test_batch")
+        label_kword = "labels"
+    elif dataset == "cifar100":
+        batches = ("train", "test")
+        label_kword = "fine_labels"
     images, labels = [], []
     for batch in batches:
         path = os.path.join(prefix, batch)
-        im, la = LoadBatch(path)
+        im, la = LoadBatch(path, label_kword=label_kword)
         images.append(im)
         labels.append(la)
     if include_labels:
